e758cd10199c555208da5e0deeb9c8a3525c7295
thefonso <thefonso@thefonso.com>
2017-12-30 13:14:36 -0800

    Event Modifiers begins

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 new Vue({
   el:'#vue-app',
   data: {
-    age: 25
+    age: 25,
+    x:0,
+    y:0
   },
   methods:{
     add:function(inc){
@@ -9,6 +11,10 @@ new Vue({
     },
     subtract:function(dec){
       this.age -= dec;
+    },
+    updateXY:function(event){
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
