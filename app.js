8e6e59e06a973d00b44cd237fe9906200e1c4f2c
thefonso <thefonso@thefonso.com>
2017-12-30 12:30:11 -0800

    Data-Binding done

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el:'#vue-app',
   data: {
     name:'Fonso',
-    job: 'Samurai'
+    job: 'Samurai',
+    website: 'http://unclefonso.com',
+    websiteTag:'<a href="http://unclefonso.com">unclefonso</a>'
   },
   methods:{
     greet:function(time_of_day){
