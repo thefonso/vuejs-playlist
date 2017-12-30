5b9212253676809c26956e2ceeae4274ba5f9f71
thefonso <thefonso@thefonso.com>
2017-12-30 15:13:45 -0800

    adjusting css and js file for begining of section

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
 new Vue({
   el:'#vue-app',
   data: {
-    age: 20,
-    a: 0,
-    b:0
-
+    available: true,
+    nearby: false
   },
   methods:{
-    // addToA: function () {
-    //   console.log('addToA');
-    //   return this.a + this.age;
-    // },
-    // addToB: function () {
-    //   console.log('addToB');
-    //   return this.b + this.age;
-    // }
+
   },
   computed:{
-    addToA: function () {
-      console.log('addToA');
-      return this.a + this.age;
-    },
-    addToB: function () {
-      console.log('addToB');
-      return this.b + this.age;
-    }
+
   }
 });
