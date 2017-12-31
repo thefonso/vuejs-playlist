1870bcf3ae8d70546ec7caeb170fff5a77738b6d
thefonso <thefonso@thefonso.com>
2017-12-31 14:33:37 -0800

    conditionals - begins

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 new Vue({
   el:'#vue-app',
   data: {
-    available: true,
-    nearby: false
+    error:false,
+    success:false
   },
   methods:{
 
   },
   computed:{
-    compClasses: function () {
-      return{
-        available: this.available,
-        nearby: this.nearby
-      }
-    }
+
   }
 });
