4a14b31b1a7314d01c55d4ec379c953702d97a62
thefonso <thefonso@thefonso.com>
2017-12-31 20:14:17 -0800

    http post - done

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,24 +1,29 @@
 <template>
   <div id="add-blog">
     <h2>Add a New Blog Post</h2>
-    <form action="">
+    <form v-if="!submitted">
       <label for="">Blog Title:</label>
       <input type="text" required v-model.lazy="blog.title">
       <label for="">Blog Content:</label>
       <textarea name="" id="" cols="30" rows="10" v-model.lazy="blog.content"></textarea>
+      <div id="checkboxes">
+        <label for="">Samuari</label>
+        <input type="checkbox" value="samuari" v-model="blog.categories"/>
+        <label for="">Wizards</label>
+        <input type="checkbox" value="wizards" v-model="blog.categories"/>
+        <label for="">Cheese</label>
+        <input type="checkbox" value="Cheese" v-model="blog.categories"/>
+      </div>
+      <label for="">Author:</label>
+      <select name="" v-model="blog.author">
+        <option v-for="author in authors">{{author}}</option>
+      </select>
+      <button v-on:click.prevent="post">Add Blog</button>
     </form>
-    <div id="checkboxes">
-      <label for="">Samuari</label>
-      <input type="checkbox" value="samuari" v-model="blog.categories"/>
-      <label for="">Wizards</label>
-      <input type="checkbox" value="wizards" v-model="blog.categories"/>
-      <label for="">Cheese</label>
-      <input type="checkbox" value="Cheese" v-model="blog.categories"/>
+    <div v-if="submitted">
+      <h3>Thanks for adding post</h3>
     </div>
-    <label for="">Author:</label>
-    <select name="" v-model="blog.author">
-      <option v-for="author in authors">{{author}}</option>
-    </select>
+
     <div id="preview">
       <h3>Preview Blog</h3>
       <p>Blog Title: {{blog.title}}</p>
@@ -30,6 +35,7 @@
       </u>
       <p>Author: {{blog.author}}</p>
     </div>
+
   </div>
 </template>
 
@@ -45,11 +51,21 @@
           categories:[],
           author:""
         },
-        authors:['UncleFonso', 'Neil Gaiman', 'Dr. Seuss']
+        authors:['UncleFonso', 'Neil Gaiman', 'Dr. Seuss'],
+        submitted:false,
       }
     },
     methods: {
-
+      post: function () {
+        this.$http.post('http://jsonplaceholder.typicode.com/posts',{
+          title: this.blog.title,
+          body: this.blog.content,
+          userID: 1
+        }).then(function(data){
+          console.log(data);
+          this.submitted = true;
+        })
+      }
     }
   }
 </script>
