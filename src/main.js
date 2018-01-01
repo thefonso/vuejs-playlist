b429c11a1a5dd67940d89b9f28a718138f5444f3
thefonso <thefonso@thefonso.com>
2018-01-01 01:17:28 -0800

    arguments, values in custom directives - done

--- src/main.js
+++ src/main.js
@@ -11,6 +11,20 @@ Vue.directive('rainbow',{
   }
 });
 
+Vue.directive('theme',{
+  bind(el, binding, vnode){
+    if(binding.value == 'wide'){
+      el.style.maxWidth = "1200px"
+    }else if(binding.value == 'narrow'){
+      el.style.maxWidth = "560px"
+    }
+    if(binding.arg == 'column'){
+      el.style.background = 'darkgray';
+      el.style.padding = '20px';
+    }
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
